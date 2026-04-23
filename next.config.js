2b27ced6934cddaa7393fd085909ca083303869d
fixl-developer <develop@fixlsolutions.com>
2026-04-23 23:38:03 +0530

    build error solved

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
         minimumCacheTTL: 60,
     },
+    onDemandEntries: {
+        maxInactiveAge: 60 * 1000,
+        pagesBufferLength: 5,
+    },
 }
 
 module.exports = nextConfig
